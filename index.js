e2f1be26a9d71c5f3d1feaa65b52aab2608cf835
Hadji_Youcef <y.hadji.ing@lagh-univ.dz>
2024-02-29 22:22:01 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-af
+let str="Sup";
+console.log(str);
+console.log(typeof str);
+
+// ? str=null;
+console.log(typeof str);
+
+str=7;
+let Obj ={
+    str: str,
+    num: 123,
+    str2: "this is way easier than is should be no?",
+};
+console.log(Obj);
+
+let array = [7,5,6,7,5];
+console.log(array);
