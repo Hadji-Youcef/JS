cf9756a75558b4561c1327c02645fb4565cb496d
Hadji_Youcef <y.hadji.ing@lagh-univ.dz>
2024-03-01 09:24:44 +0100

    new file:   .gitignore 	new file:   .tours/index.tour 	new file:   draw.drawio 	new file:   package.json

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let str="Sup";
+let str = "Player";
 console.log(str);
 console.log(typeof str);
 
 // ? str=null;
 console.log(typeof str);
 
-str=7;
-            let Obj ={
-    str: str,
-    num: 123,
-    str2: "this is way easier than is should be no?",
+str = 7;
+let Obj = {
+  str: str,
+  num: 123,
+  str2: "this is way easier than is should be no?",
 };
 console.log(Obj);
 
-let array = [7,5,6,7,5];
+let array = [7, 5, 6, 7, 5];
 console.log(array);
